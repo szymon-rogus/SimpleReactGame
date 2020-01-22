a2aaf57ad6a5e46e2be259119f3cf93fc9e511fb
szymon-rogus <sszymek98@gmail.com>
2020-01-22 23:42:21 +0100

    Updates

---
+++ precache-manifest.6a66f43acefc67e8b96845c84a59a1ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cb9d15c88d0fb6e3bb06715193aa07",
+    "url": "/SimpleReactGame/index.html"
+  },
+  {
+    "revision": "26c3d18bb9e5c861f8d3",
+    "url": "/SimpleReactGame/static/css/main.6944ccff.chunk.css"
+  },
+  {
+    "revision": "e806e8a3ba3ef66ec14f",
+    "url": "/SimpleReactGame/static/js/2.4b4fa182.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SimpleReactGame/static/js/2.4b4fa182.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26c3d18bb9e5c861f8d3",
+    "url": "/SimpleReactGame/static/js/main.1b8ebbe6.chunk.js"
+  },
+  {
+    "revision": "43a3b49a77f9bb5dc143",
+    "url": "/SimpleReactGame/static/js/runtime-main.d4d1f054.js"
+  },
+  {
+    "revision": "bff0f435115f19cf276fc263ea294dac",
+    "url": "/SimpleReactGame/static/media/pytajnik.bff0f435.jpg"
+  }
+]);
